fac590df9271e5366d686765e5a0a7a040fe89f4
sweetfamine <186402465+sweetfamine@users.noreply.github.com>
2025-11-15 12:08:13 +0100

    added basic Taskmanager funktions

--- src/components/windows/TaskManagerWindow.vue
+++ src/components/windows/TaskManagerWindow.vue
@@ -2,29 +2,15 @@
   <div class="window-inner">
     <h3>Task-Manager</h3>
     <ul>
-      <li>Bits – 24</li>
-      <li>Cores – 3</li>
-      <li>Chaos – 1% CPU</li>
+      <li>Bits – {{ gameState.bits }}</li>
+      <li>Cores – {{ gameState.cores }}</li>
+      <li>Chaos – {{ gameState.chaos }}%</li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
+import { gamestate as gameState } from '../../store/gamestate'
 </script>
 
-<style scoped>
-.window-inner h3 {
-  margin: 0 0 6px;
-  font-size: 13px;
-}
-
-.window-inner ul {
-  margin: 0;
-  padding-left: 16px;
-}
-
-.window-inner li {
-  font-size: 12px;
-  line-height: 1.4;
-}
-</style>
+<style scoped src="./TaskManagerWindow.css"></style>
